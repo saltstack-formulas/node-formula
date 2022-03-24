1d97949be7532fbbafd040c334846ac869a1be4b
Imran Iqbal <iqbalmy@hotmail.com>
2022-03-24 06:07:59 +0000

    feat(openbsd): add support to work with the `default` suite

--- test/integration/default/controls/environ_spec.rb
+++ test/integration/default/controls/environ_spec.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
+# Overide by Platform
+root_group =
+  case platform[:family]
+  when 'bsd'
+    'wheel'
+  else
+    'root'
+  end
+
 control 'node configuration environment' do
   title 'should match desired lines'
 
   describe file('/etc/default/node.sh') do
     it { should be_file }
     it { should be_owned_by 'root' }
-    it { should be_grouped_into 'root' }
+    it { should be_grouped_into root_group }
     its('mode') { should cmp '0640' }
   end
 end
